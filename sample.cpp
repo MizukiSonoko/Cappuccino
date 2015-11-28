5230d8d36c2f4748e07cc20c37268e438948c806
mizuki.sonoko <mizuki.sonoko@gmail.com>
2015-11-28 15:31:06 +0900

    [Update] sample.cpp
    add page.

--- sample.cpp
+++ sample.cpp
@@ -6,11 +6,18 @@ int main(int argc, char *argv[]) {
 	Cappuccino::document_root("html");
 
 	Cappuccino::add_route("/", [&](Cappuccino::Request* req) -> Cappuccino::Response{
-		Logger::d("Call function");
 		auto response = Cappuccino::Response(req->protocol(), Cappuccino::Response::FILE);
 		response.set_filename("index.html");
 		return response;
 	});
+
+
+	Cappuccino::add_route("/chino", [&](Cappuccino::Request* req) -> Cappuccino::Response{
+		auto response = Cappuccino::Response(req->protocol(), Cappuccino::Response::FILE);
+		response.set_filename("chino.html");
+		return response;
+	});
+
 	Cappuccino::run();
 
 }
