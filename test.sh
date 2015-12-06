701312e832e732a1ccbebffb29f572c32657b837
mizuki.sonoko <mizuki.sonoko@gmail.com>
2015-12-07 01:58:33 +0900

    [Fix] set prefix 'localhost' to test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-TARGET=$1
+TARGET=localhost:$1
 for i in {1..250};
-do 
+do
 	curl -s  -o /dev/null $TARGET  -w  '%{speed_download}\n' ;
 done \
 |  awk '{sum += ($1/1024/1024) ; count +=1; } END  {print sum/count*8 }'
