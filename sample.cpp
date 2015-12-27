5a912dd1f2902aa607583b45e25f1b8e93ef9bce
mizuki.sonoko <mizuki.sonoko@gmail.com>
2015-12-27 02:50:50 -0800

    [Add] json sample.

--- sample.cpp
+++ sample.cpp
@@ -1,17 +1,33 @@
 #include "cappuccino.h"
 
 int main(int argc, char *argv[]) {
-	using namespace Cappuccino;
 	Cappuccino::Cappuccino(argc, argv);
-	
-	Cappuccino::add_route("/",[](Request* request) -> Response{
-		auto response = Cappuccino::ResponseBuilder()
+	Cappuccino::add_static_root("public");
+	Cappuccino::add_view_root("html");
+
+	Cappuccino::add_route("/", [](Cappuccino::Request* req) -> Cappuccino::Response{
+		return Cappuccino::ResponseBuilder(req)
 					.status(200,"OK")
-					.file("html/index.html")
+					.file("index.html")
 					.build();
-		return response;
 	});
 
-	run();
+	Cappuccino::add_route("/hello", [](Cappuccino::Request* req) -> Cappuccino::Response{
+		return Cappuccino::ResponseBuilder(req)
+					.status(200,"OK")
+					.text("Hello world!")
+					.build();
+	});
+
+	Cappuccino::add_route("/json", [&](Cappuccino::Request* req) -> Cappuccino::Response{
+		return Cappuccino::ResponseBuilder(req)
+					.status(200,"OK")
+					.header_param("Content-type","json")
+					.file("sample.json")
+					.build();
+	});
+
+	Cappuccino::run();
+
 	return 0;
 }
