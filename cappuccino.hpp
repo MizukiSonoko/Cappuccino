7350aed9a962a086a78642048c91af97308b0e42
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-02-25 17:41:57 +0900

    [Fix] No error.

--- cappuccino.hpp
+++ cappuccino.hpp
@@ -31,101 +31,92 @@
 
 
 
+
 namespace Cappuccino{
 	
-
-	using namespace std;
-	using string = string;
-
 	struct {
 		int port_{ 1204 };
 		int sockfd_{ 0 };
 		int sessionfd_{ 0 };
 	    fd_set mask1fds, mask2fds;
 
-		shared_ptr<string> view_root_{ "" };
-		shared_ptr<string> static_root_{ "public" };
+		std::shared_ptr<std::string> view_root;
+		std::shared_ptr<std::string> static_root;
+
 	} context;
+
 
 	namespace signal_utils{
 
 		void signal_handler(int signal){
-			Logger::i("\nserver terminated!\n");		
 			close(context.sessionfd_);
 			close(context.sockfd_);
 			exit(0);
 		}
 
-		void signal_handler_child(int signal){
+		static void signal_handler_child(int SignalName){
 			while(waitpid(-1,NULL,WNOHANG)>0){}
-	        signal(SIGCHLD, Cappuccino::signal_utils::signal_handler_child);
+	        signal(SIGCHLD, signal_utils::signal_handler_child);
 		}
 
 		void init_signal(){
-			if (signal(SIGINT, Cappuccino::signal_utils::signal_handler) == SIG_ERR) {
-				Logger::e("signal setting error");
+			if (signal(SIGINT, signal_utils::signal_handler) == SIG_ERR) {
 				exit(1);
 			}
-			if (signal(SIGCHLD, Cappuccino::signal_utils::signal_handler_child) == SIG_ERR) {
-				Logger::e("signal setting error");
+			if (signal(SIGCHLD, signal_utils::signal_handler_child) == SIG_ERR) {
 				exit(1);
 			}
 		}
 	}
+
+
+	void init_socket(){
+	    struct sockaddr_in server;
+		if ((context.sockfd_ = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+			exit(EXIT_FAILURE);
+		}
+		memset( &server, 0, sizeof(server));
+		server.sin_family = AF_INET;	
+		server.sin_addr.s_addr = INADDR_ANY;
+		server.sin_port = htons(context.port_);
+
+		char opt = 1;
+		setsockopt(context.sockfd_, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt));
+
+		int temp = 1;
+  		if(setsockopt(context.sockfd_, SOL_SOCKET, SO_REUSEADDR,
+	            &temp, sizeof(temp))){
+		}
+		
+		if (bind(context.sockfd_, (struct sockaddr *) &server, sizeof(server)) < 0) {
+			exit(EXIT_FAILURE);
+		}
+
+		if(listen(context.sockfd_,  MAX_LISTEN) < 0) {
+			exit(EXIT_FAILURE);
+		}
+
+	    FD_ZERO(&context.mask1fds);
+	    FD_SET(context.sockfd_, &context.mask1fds);
+	}
+
+
+	using namespace std;
+	using string = string;
 
 	void option(int argc, char *argv[]) noexcept{
 		char result;
 		while((result = getopt(argc,argv,"dp:")) != -1){
 			switch(result){
 			case 'd':
-				Logger::debug_ = true;
 				break;
 			case 'p':
-				port_ = atoi(optarg);
+				context.port_ = atoi(optarg);
 				break;
 			}
 		}
-		if(Logger::debug_){
-			Logger::i("mode: debug");
-		}else{			
-			Logger::i("mode: product");
-		}
-		Logger::d("port:" + std::to_string(port_));
 	}
 
-	void init_socket(){
-	    struct sockaddr_in server;
-		if ((sockfd_ = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-			Logger::e("socket create error");
-			exit(EXIT_FAILURE);
-		}
-		memset( &server, 0, sizeof(server));
-		server.sin_family = AF_INET;	
-		server.sin_addr.s_addr = INADDR_ANY;
-		server.sin_port = htons(port_);
-
-		char opt = 1;
-		setsockopt(sockfd_, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt));
-
-		int temp = 1;
-  		if(setsockopt(sockfd_, SOL_SOCKET, SO_REUSEADDR,
-	            &temp, sizeof(temp))){
-		    Logger::e("setsockopt() failed");
-		}
-		
-		if (bind(sockfd_, (struct sockaddr *) &server, sizeof(server)) < 0) {
-			Logger::e("socket bind error");
-			exit(EXIT_FAILURE);
-		}
-
-		if(listen(sockfd_,  MAX_LISTEN) < 0) {
-			Logger::e("listen error");
-			exit(EXIT_FAILURE);
-		}
-
-	    FD_ZERO(&context->mask1fds);
-	    FD_SET(sockfd_, &context->mask1fds);
-	}
 
 	namespace Regex{
 		std::vector<string> findParent(string text) noexcept{
@@ -155,38 +146,24 @@ namespace Cappuccino{
 	std::regex re( R"(<\w+>)");
     std::smatch m;
 #endif    
+    class Response{
+      public:
+      	operator string(){
+      		return "";
+      	}
+    };
 
-	RuleResponse create_response(char* req) noexcept{
-		std::unique_ptr<Request> request = Request().factory(string(req));
-
-		auto pos(routes_.find(request->url()));
-		if( pos != routes_.end()){
-			auto res = pos->second(request.get());
-			//Logger::d("-------");
-			//Logger::d(res);
-			return res;
-		}		
-		// static
-		pos = static_routes_.find(request->url());
-		if( pos != static_routes_.end()){
-			return pos->second(request.get());
-		}		
-
-		auto posir = other_routes_.find(404);
-		if( posir != other_routes_.end()){
-			return posir->second(request.get());
-		}
+	Response create_response(char* req) noexcept{
 		return Response();
 	}
 
-	Rulestring receive_process(int sessionfd){
+	string receive_process(int sessionfd){
 		char buf[BUF_SIZE] = {};
 		char method[BUF_SIZE] = {};
 		char url[BUF_SIZE] = {};
 		char protocol[BUF_SIZE] = {};
 
 		if (recv(sessionfd, buf, sizeof(buf), 0) < 0) {
-			Logger::e("receive error!");
 			exit(EXIT_FAILURE);
 		}
 		bool isbody = false;
@@ -201,14 +178,13 @@ namespace Cappuccino{
 				memset(&buf, 0, sizeof(buf));
 			}
 		} while (read(sessionfd, buf+strlen(buf), sizeof(buf) - strlen(buf)) > 0);
-		return create_response(buf);
+		return create_response(buf);	
 	}
 	
 	time_t client_info[FD_SETSIZE];
 
-	void load(string directory,string filename) noexcept{
+	void load(string directory, string filename) noexcept{
 		if(filename == "." || filename == "..") return;
-
 		if(filename!="")
 			directory += "/" + filename;
 		DIR* dir = opendir(directory.c_str());
@@ -222,7 +198,7 @@ namespace Cappuccino{
 		    }
 		    closedir(dir);
 		}else{
-			Logger::i(directory);
+			/*
 			auto static_file = Cappuccino::FileLoader(directory);
 			static_file.preload();
 			static_routes_.insert( make_pair(
@@ -234,19 +210,23 @@ namespace Cappuccino{
 							.build();
 					}
 				));
+			*/
 		}
 	}
 
 	void load_static_files() noexcept{
-		load(static_root_,"");
+		load(*context.static_root,"");
 	}
 
 	void run(){
+
+		context.view_root = make_shared<string>("");		
+		context.static_root = make_shared<string>("public");
+
 		init_socket();
 		signal_utils::init_signal();
 		load_static_files();
 
-		Logger::i("Running on http://localhost:" + std::to_string(port_) + "/");
 
 	    int cd[FD_SETSIZE];
 		struct sockaddr_in client;
@@ -261,31 +241,30 @@ namespace Cappuccino{
 	        tv.tv_sec = 0;
 	        tv.tv_usec = 0;
 
-	        memcpy(&mask2fds, &context->mask1fds, sizeof(context->mask1fds));
+	        memcpy(&context.mask2fds, &context.mask1fds, sizeof(context.mask1fds));
 
-	        int select_result = select(FD_SETSIZE, &mask2fds, (fd_set *)0, (fd_set *)0, &tv);
+	        int select_result = select(FD_SETSIZE, &context.mask2fds, (fd_set *)0, (fd_set *)0, &tv);
 	        if(select_result < 1) {
 	            for(fd = 0; fd < FD_SETSIZE; fd++) {
 	                if(cd[fd] == 1) {
 	                    close(fd);
-	                    FD_CLR(fd, &context->mask1fds);
+	                    FD_CLR(fd, &context.mask1fds);
 	                    cd[fd] = 0;
 	                }
 	            }
 	            continue;
 	        }
 	        for(fd = 0; fd < FD_SETSIZE; fd++){
-	            if(FD_ISSET(fd,&mask2fds)) {
-	                if(fd == sockfd_) {
+	            if(FD_ISSET(fd,&context.mask2fds)) {
+	                if(fd == context.sockfd_) {
 	                	memset( &client, 0, sizeof(client));
 						int len = sizeof(client);
-	                    int clientfd = accept(sockfd_, 
-	                        (struct sockaddr *)&client,(socklen_t *) &len);
-	                        FD_SET(clientfd, &context->mask1fds);
+	                    int clientfd = accept(context.sockfd_, (struct sockaddr *)&client,(socklen_t *) &len);
+                        FD_SET(clientfd, &context.mask1fds);
 	                }else {
 	                    if(cd[fd] == 1) {
 	                        close(fd);
-	                        FD_CLR(fd, &context->mask1fds);
+	                        FD_CLR(fd, &context.mask1fds);
 	                        cd[fd] = 0;
 	                    } else {
 							string response = receive_process(fd);
@@ -299,8 +278,8 @@ namespace Cappuccino{
 	}
 
 	void Cappuccino(int argc, char *argv[]) {
-		port_ = 1204;
-		set_argument_value(argc, argv);
+		context.port_ = 1204;
+		option(argc, argv);
 	}
 };
 
