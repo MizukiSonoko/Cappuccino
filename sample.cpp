d58f97cf7549ddf736d63fa235cb7038d9eb670a
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-03-09 20:03:47 +0900

    [Apply] Test

--- sample.cpp
+++ sample.cpp
@@ -1,15 +1,17 @@
 #include "cappuccino.hpp"
 
 int main(int argc, char *argv[]){
-/*
+
+#ifdef TEST
 	Cocoa::testOpenFile();
 	Cocoa::testOpenInvalidFile();
-*/
+#else
 	Cappuccino::Cappuccino(argc, argv);	
 	Cappuccino::root("html");
 	Cappuccino::route("/",[](std::shared_ptr<Cappuccino::Request> request) -> Cappuccino::Response{
 		return *Cappuccino::Response(request).status(200)->message("OK")->file("index.html");
 	});
 	Cappuccino::run();
+#endif
 	return 0;
 }
